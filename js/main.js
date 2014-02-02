c9c053520c7ad124f79106d94d61ff61c2e094a6
Andrew <im@andreystarkov.ru>
2014-02-02 23:08:50 +0600

    stable 2

--- js/main.js
+++ js/main.js
@@ -5,4 +5,20 @@
             interval: 7000
        })
 
+
+      $('.btn-catalog').click( function(){
+            $(this).addClass('active');
+            $('.popup-catalog').fadeToggle();
+       });
+
+
+      var allPanels = $('.accordion > dd').hide();
+
+      $('.accordion > dt > a').click(function() {
+        allPanels.slideUp();
+        $(this).parent().next().slideDown();
+        return false;
+      });
+
+
     });
